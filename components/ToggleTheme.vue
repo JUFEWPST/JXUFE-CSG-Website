3d0ffaff14bd79d50073b5a667215486e8f5249d
糖糖毬 <TangTangChu@outlook.com>
2025-07-03 13:15:01 +0800

    适配tailwindcssv4的最新实践，调整使用暗色的方式

---
+++ components/ToggleTheme.vue
@@ -0,0 +1,12 @@
+<template>
+    <button class=" rounded-full w-[40px] h-[40px]" @click="toggleTheme">
+        <MoonIcon v-if="isDark"></MoonIcon>
+        <SunIcon v-else></SunIcon>
+    </button>
+</template>
+
+<script lang="ts" setup>
+import { SunIcon } from '@heroicons/vue/24/outline';
+import { MoonIcon } from '@heroicons/vue/24/outline';
+const { isDark, toggleTheme } = useTheme();
+</script>
