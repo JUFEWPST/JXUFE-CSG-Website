a6d1f42ebe2472ca08035b2346659a9b59a8cacb
糖糖毬 <TangTangChu@outlook.com>
2025-07-10 23:23:40 +0800

    介绍页建设 (#8)
    * 初步建设介绍页;加了个翻转控件
    
    * 添加了相关荣誉;为“加入我们”添加了“名言名句”
    
    * 修改首页归档入口文字；添加介绍页入口
    
    * 优化部分动效

---
+++ components/FlipToggle.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="flip-container" :style="containerStyle">
+        <div class="flipper" :class="{ 'flipped': isFlipped }" @click="handleClick">
+            <div class="front">
+                <slot name="front"></slot>
+            </div>
+            <div class="back">
+                <slot name="back"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    duration: {
+        type: Number,
+        default: 600
+    },
+    clickable: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const emit = defineEmits(['flip', 'update:modelValue']);
+
+const isFlipped = ref(false);
+
+const containerStyle = computed(() => ({
+    cursor: props.clickable ? 'pointer' : 'auto'
+}));
+
+const transitionDuration = computed(() => `${props.duration}ms`);
+
+const toggleFlip = () => {
+    isFlipped.value = !isFlipped.value;
+    emit('flip', isFlipped.value);
+    emit('update:modelValue', isFlipped.value);
+};
+
+const handleClick = () => {
+    if (props.clickable) {
+        toggleFlip();
+    }
+};
+
+defineExpose({
+    toggleFlip
+});
+</script>
+
+<style scoped>
+.flip-container {
+    perspective: 1000px;
+    margin: 0 auto;
+}
+
+.flipper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transition: transform v-bind(transitionDuration);
+    transform-style: preserve-3d;
+}
+
+.flipper.flipped {
+    transform: rotateY(180deg);
+}
+
+.front,
+.back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.back {
+    transform: rotateY(180deg);
+}
+</style>
+
