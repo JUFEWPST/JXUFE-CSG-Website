7ab99d578db3b2b0d961702657060d7d30b765a0
糖糖毬 <TangTangChu@outlook.com>
2025-07-14 23:12:28 +0800

    把app.vue的导航栏与页脚组件移动至layouts中

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import NavBar from '@/components/NavBar.vue'
+import PageFooter from '@/components/PageFooter.vue'
+</script>
+
+<template>
+    <div class="layout-wrapper">
+        <NavBar />
+        <slot />
+        <PageFooter />
+    </div>
+</template>
