eb5709f47818dbe8778a0583c2eb52b55a1743ab
糖糖毬 <TangTangChu@outlook.com>
2025-07-16 23:47:24 +0800

    测试页更新

--- pages/test.vue
+++ pages/test.vue
@@ -13,9 +13,9 @@ const testNotifications = () => {
     })
 
     notify({
-        message: '这是一个信息通知',
+        message: '我们不会使用任何Cookie',
         type: NotificationType.INFO,
-        timeout: 5000
+        timeout: 7000
     })
 
     notify({
@@ -56,7 +56,11 @@ const triggerNotification = (type: NotificationType, message: string) => {
                     class="px-3 py-1 bg-green-500 text-white rounded hover:bg-green-600 transition-colors">
                     成功通知
                 </button>
-                <button @click="triggerNotification(NotificationType.INFO, '我们不会使用Cookie')"
+                <button @click="notify({
+                    message: '我们不会使用任何Cookie',
+                    type: NotificationType.INFO,
+                    timeout: 7000
+                })"
                     class="px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors">
                     我们不会使用Cookie
                 </button>
