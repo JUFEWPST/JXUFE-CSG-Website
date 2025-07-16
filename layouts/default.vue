896437b9f14e99c34e1cb2a7b046c84cee38ddfb
糖糖毬 <TangTangChu@outlook.com>
2025-07-16 23:26:53 +0800

    添加了每一次新会话显示notice的功能

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,14 @@ import NavBar from '@/components/NavBar.vue'
 import PageFooter from '@/components/PageFooter.vue'
 import { useNotification } from '@/composables/useNotification'
 import AnzuNotification from '~/components/AnzuNotification.vue'
+import { useFetchNotice } from '@/composables/useFetchNotice'
+import { useOncePerSession } from '@/composables/useOncePerSession'
 const { notificationRef } = useNotification()
+if (import.meta.client) {
+    useOncePerSession('init-notice', () => {
+        useFetchNotice()
+    })
+}
 </script>
 
 <template>
