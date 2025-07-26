2f7f90c2051265f15c047018fabd8d2c98921922
糖糖毬 <TangTangChu@outlook.com>
2025-07-26 21:49:45 +0800

    完善首页 (#23)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,13 @@ export default defineNuxtConfig({
       ],
       htmlAttrs: {
         lang: 'zh-CN'
-      }
+      },
+      meta: [
+        {
+          name: "江西财经大学网络安全协会",
+          content: "江西财经大学网络安全协会成立于2016年9月，协会的宗旨是想全校师生普及网络安全知识、培养网络安全技术人才、互相交流、共同进步",
+        },
+      ],
     }
   }
 })
