4940ada892b838f7fff10e6385dacf1adc6d445d
糖糖毬 <TangTangChu@outlook.com>
2025-08-04 18:13:41 +0800

    历届负责人展示页面

---
+++ pages/about/leaders.vue
@@ -0,0 +1,11 @@
+<template>
+    <main class="mt-15 mb-2 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <h1 class="font-bold text-4xl text-center">历届负责人</h1>
+        <LeadersList :leaders="leadersData" />
+    </main>
+
+</template>
+<script setup lang="ts">
+import LeadersList from '~/components/LeadersList.vue';
+import { leadersData } from '~/data/leadersData';
+</script>
