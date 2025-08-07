631bf04de01edbd9d661235a285b37daf0fda3fb
糖糖毬 <TangTangChu@outlook.com>
2025-08-07 18:47:06 +0800

    新增荣誉

--- data/leadersData.ts
+++ data/leadersData.ts
@@ -15,8 +15,8 @@ export const leadersData = [
         year: "2024",
         message: "寄语",
         achievements: [
-            "成就"
+            '第21、22届全国大学生信息安全与对抗技术竞赛国家一等奖', '2024睿抗机器人开发者大赛(RAICOM)全国总决赛网络安全竞赛项目国家二等奖', '第十八届全国大学生信息安全竞赛（创新实践能力赛）暨第二届“长城杯”铁人三项赛（防护赛）半决赛三等奖', '2024睿抗机器人开发者大赛(RAICOM)江西省网络安全竞赛项目二等奖', '蓝桥杯网安赛道省级二等奖', '江财网安全能神'
         ],
-        avatar: 'http://47.99.92.213/images/excellent_member/xzy.png'
+        avatar: '/avatar/qyy.avif'
     },
 ]
