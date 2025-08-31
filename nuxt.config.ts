4cc74e9225360c027c879a01a236f2503aeb322f
糖糖毬 <TangTangChu@outlook.com>
2025-08-31 17:08:00 +0800

    修正name错误

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,7 +20,7 @@ export default defineNuxtConfig({
       },
       meta: [
         {
-          name: "江西财经大学网络安全协会",
+          name: "description",
           content: "江西财经大学网络安全协会成立于2016年9月，协会的宗旨是想全校师生普及网络安全知识、培养网络安全技术人才、互相交流、共同进步",
         },
         {
